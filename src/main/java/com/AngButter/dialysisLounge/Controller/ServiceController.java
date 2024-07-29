efc9186eed98a2a689a80722c4643d7bdaedf113
릴우지버트이복동생 <168535072+lilgyeongwonvert@users.noreply.github.com>
2024-07-30 00:19:54 +0900

    Add files via upload
    11

---
+++ src/main/java/com/AngButter/dialysisLounge/Controller/ServiceController.java
@@ -0,0 +1,35 @@
+package com.AngButter.dialysisLounge.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/service")
+@Controller
+public class ServiceController {
+
+    @GetMapping("/management")
+    public String management() {
+        return "lifeManagement";
+    }
+
+    @GetMapping("/emergency")
+    public String emergency() {
+        return "emergency";
+    }
+
+    @GetMapping("/education-magazine")
+    public String educationmagazine() {
+        return "education-magazine";
+    }
+
+    @GetMapping("/education-video")
+    public String educationvideo() {
+        return "education-video";
+    }
+
+    @GetMapping("/map")
+    public String map() {
+        return "map";
+    }
+}
